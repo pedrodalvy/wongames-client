018e98bf5738a2facb91ed68fe2bca2adca292b4
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-06 15:11:17 -0400

    Add option to hide Logo text on mobile

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import { LogoProps } from './index';
+import media from 'styled-media-query';
 
 const wrapperModifiers = {
   normal: () => css`
@@ -11,12 +12,29 @@ const wrapperModifiers = {
     width: 20rem;
     height: 5.9rem;
   `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
+  `,
 };
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && wrapperModifiers[size]};
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile};
   `}
 `;
