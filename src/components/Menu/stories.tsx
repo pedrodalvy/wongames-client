8e38361136e424452f45eda7d0ab8826905c94a7
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-07 11:34:16 -0400

    Add Menu mobile component

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/react';
+import Menu from '.';
+
+export default {
+  title: 'Menu',
+  component: Menu,
+} as Meta;
+
+export const Default: Story = () => <Menu />;
+
+Default.parameters = {
+  backgrounds: {
+    default: 'dark',
+  },
+  viewport: {
+    defaultViewport: 'mobile1',
+  },
+};
