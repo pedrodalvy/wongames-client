8897079ea8c9bca021fa8d77c5bf4e986a3dc419
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-07 11:45:11 -0400

    Add tests to Menu component

---
+++ src/components/Menu/test.tsx
@@ -0,0 +1,15 @@
+import Menu from '.';
+import { renderWithTheme } from 'utils/tests/helpers';
+import { screen } from '@testing-library/react';
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />);
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
+
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument();
+  });
+});
