34a68f6a2871b702c550c452db7be2a7fe3af643
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-07 20:57:53 -0400

    Add tests to open/close menu component

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,6 +1,6 @@
 import Menu from '.';
 import { renderWithTheme } from 'utils/tests/helpers';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 describe('<Menu />', () => {
   it('should render the menu', () => {
@@ -11,5 +11,34 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
 
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument();
+  });
+
+  test('makes sure the mobile menu starts hidden', () => {
+    renderWithTheme(<Menu />);
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true });
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
+  });
+
+  test('ensures that when clicking in the Open Menu, the mobile menu opens', () => {
+    renderWithTheme(<Menu />);
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true });
+
+    fireEvent.click(screen.getByLabelText(/open menu/i));
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false');
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 });
+  });
+
+  test('ensures that when clicking in the Close Menu, the mobile menu closes', () => {
+    renderWithTheme(<Menu />);
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true });
+
+    fireEvent.click(screen.getByLabelText(/open menu/i));
+    fireEvent.click(screen.getByLabelText(/close menu/i));
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true');
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 });
   });
 });
