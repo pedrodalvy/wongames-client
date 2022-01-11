1c0f4caa2238ab22eaef8b7c44503730f5446e1a
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-11 16:00:25 -0400

    Add test on Button component to ensure it renders as a link

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -51,4 +51,15 @@ describe('<Button />', () => {
     expect(screen.getByText(/buy now/i)).toBeInTheDocument();
     expect(screen.getByTestId('icon')).toBeInTheDocument();
   });
+
+  it('should render button as a link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    );
+
+    const button = screen.getByRole('link', { name: /buy now/i });
+    expect(button).toHaveAttribute('href', '/link');
+  });
 });
