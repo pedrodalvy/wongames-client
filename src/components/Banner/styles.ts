5954eb4e0168592f5116d0a2f26e912b5cf30670
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-17 15:36:34 -0400

    Fix Ribbon style on small Banners

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,3 +1,5 @@
+import * as RibbonStyles from 'components/Ribbon/styles';
+
 import styled, { css } from 'styled-components';
 
 import media from 'styled-media-query';
@@ -8,6 +10,16 @@ type ImageProprs = {
 
 export const Wrapper = styled.main`
   position: relative;
+
+  ${media.lessThan('large')`
+    ${RibbonStyles.Wrapper} {
+      right: 0;
+
+      &::before {
+        display: none;
+      }
+    }
+  `}
 
   ${media.greaterThan('medium')`
     box-shadow: 0 0.4rem 0.5rem 0 rgba(0, 0, 0, 0.2);
