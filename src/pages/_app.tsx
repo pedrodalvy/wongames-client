9fb18b2bc312a23fdfdf6cd1625d6a681d7381d3
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-21 14:59:12 -0400

    Update _app file with Won Games page info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,16 +8,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Boilerplate Next.js</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
-        />
-        GlobalStyles
+        <meta name="description" content="The best Game Stores in the world!" />
       </Head>
+
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
