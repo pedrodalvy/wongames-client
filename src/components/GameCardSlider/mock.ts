08133365db5a725b686d2cc7f073530fa670ae99
Pedro Dalvy <pedrodalvy@outlook.com>
2022-01-21 14:56:38 -0400

    Refact GameCardSlider stories data to mock file

---
+++ src/components/GameCardSlider/mock.ts
@@ -0,0 +1,46 @@
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/301x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/302x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/303x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/304x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/305x145',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+];
+
+export default items;
--- src/components/GameCardSlider/stories.tsx
+++ src/components/GameCardSlider/stories.tsx
@@ -1,51 +1,7 @@
 import { Story, Meta } from '@storybook/react';
 import { GameCardProps } from 'components/GameCard';
 import GameCardSlider from 'components/GameCardSlider';
-
-const items = [
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/301x141',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/302x142',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/303x143',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/304x144',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/305x145',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00',
-  },
-];
+import items from 'components/GameCardSlider/mock';
 
 export default {
   title: 'GameCardSlider',
